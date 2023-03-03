480a77bf1841e0ba0a7c4e1ae88cac58e288b07d
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-03 12:44:25 -0500

    Extract methods into Game class.

--- hangman.cpp
+++ hangman.cpp
@@ -2,18 +2,34 @@
 
 class Game{
 	public:
-		int totalGuesses;
-		Game(int numberOfGuesses){
-			totalGuesses = numberOfGuesses;
+		void getNumberOfGuesses(){
+			std::string userInputGuesses;
+			std::cout << "How many guesses would you like to have? " << std::flush;
+			std::getline(std::cin,userInputGuesses);
+	    	int convertedUserInput = std::stoi(userInputGuesses);
+			this->setTotalGuesses(convertedUserInput);
+		} 
+		void setTotalGuesses(int guesses){
+			this->totalGuesses = guesses;
 		}
+		std::string getTotalGuesses(){
+			std::cout << "TO STRING: " << std::to_string(this->totalGuesses) << std::endl;
+			return std::to_string(this->totalGuesses);
+		}
+
+	private:
+		int totalGuesses;
+
+};
+
+class Word{
+	public:
+		std::string randomWord = "hello world";
 };
 
 int main(){
-	std::string userInputGuesses;
-	std::cout << "How many guesses would you like to have? " << std::flush;
-	std::getline(std::cin,userInputGuesses);
-	int convertedUserInput = std::stoi(userInputGuesses);
-	Game hangman(convertedUserInput);
-	std::cout << hangman.totalGuesses << std::endl;
+	Game hangman;
+	hangman.getNumberOfGuesses();
+	hangman.getTotalGuesses();
 	return 0;
 }
