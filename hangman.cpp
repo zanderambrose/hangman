62b42b0695c0fa6c31a7a363c46558436c1ca16b
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-03 12:56:25 -0500

    Test auto formatter.

--- hangman.cpp
+++ hangman.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
 
-class Game {
-	public:
-		void getNumberOfGuessesFromUser(){
-			std::string userInputGuesses;
-			std::cout << "How many guesses would you like to have? " << std::flush;
-			std::getline(std::cin,userInputGuesses);
-	    	int convertedUserInput = std::stoi(userInputGuesses);
-			this->setTotalGuesses(convertedUserInput);
-		} 
-		void setTotalGuesses(int guesses){
-            this->totalGuesses -= guesses;
-			if(this->totalGuesses > 0){
-			}
+class Game
+{
+public:
+	void getNumberOfGuessesFromUser()
+	{
+		std::string userInputGuesses;
+		std::cout << "How many guesses would you like to have? " << std::flush;
+		std::getline(std::cin, userInputGuesses);
+		int convertedUserInput = std::stoi(userInputGuesses);
+		this->setTotalGuesses(convertedUserInput);
+	}
+	void setTotalGuesses(int guesses)
+	{
+		if (this->totalGuesses > 0)
+		{
+			this->totalGuesses -= guesses;
 		}
-		std::string getTotalGuesses(){
-			std::cout << "TO STRING: " << std::to_string(this->totalGuesses) << std::endl;
-			return std::to_string(this->totalGuesses);
-		}
+	}
+	std::string getTotalGuesses()
+	{
+		std::cout << "TO STRING: " << std::to_string(this->totalGuesses) << std::endl;
+		return std::to_string(this->totalGuesses);
+	}
 
-	private:
-		int totalGuesses;
-
+private:
+	int totalGuesses;
 };
 
-class Word {
-	public:
-		std::string randomWord = "hello world";
+class Word
+{
+public:
+	std::string randomWord = "hello world";
 };
 
-class Guess {
-
+class Guess
+{
 };
 
-int main(){
+int main()
+{
 	Game hangman;
 	hangman.getNumberOfGuessesFromUser();
 	hangman.getTotalGuesses();
