c5d85568c8ccb93d2ccf05f9dadc1c7fb83054fe
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-03 15:18:23 -0500

    WIP.

--- hangman.cpp
+++ hangman.cpp
@@ -2,44 +2,6 @@
 #include <vector>
 #include <fstream>
 
-class Game
-{
-public:
-	void run()
-	{
-		std::cout << "Welcome to hangman!  You will have 10 guesses to find our word! Good luck!" << std::endl;
-		while (guessesLeft > 0)
-		{
-			this->printGameStatus();
-			this->printGuessesLeft();
-			this->guessesLeft--;
-		}
-	}
-
-private:
-	int guessesLeft = 10;
-	bool isTotalGuessesInitialized = false;
-	std::vector<std::string> display{"  | ", "  | ", " [ ", "]", "  | ", "-- ", "-- ", "  | ", " /", " \\"};
-	void printGameStatus()
-	{
-		for (int i = 0; i < this->display.size(); i++)
-		{
-			if (i != 2 && i != 5 && i != 8)
-			{
-				std::cout << this->display[i] << std::endl;
-			}
-			else
-			{
-				std::cout << this->display[i];
-			}
-		}
-	}
-	void printGuessesLeft()
-	{
-		std::cout << "Guesses left: " << this->guessesLeft << std::endl;
-	}
-};
-
 class Word
 {
 public:
@@ -56,9 +18,16 @@ public:
 		this->randomWord = randomWordVector[randomNumber];
 	}
 
-	bool guessLetter(char letter)
+	bool guessLetter()
 	{
-		return true;
+		std::cout << "Select a letter: ";
+		std::string guessedLetter;
+		std::getline(std::cin, guessedLetter);
+		if (guessedLetter.size() > 1)
+		{
+			std::cout << "You can only guess one letter!  Try again." << std::endl;
+		}
+		return false;
 	}
 
 	void printWordStatus()
@@ -68,14 +37,59 @@ public:
 
 private:
 	std::string randomWord;
+	std::vector<char> guessedLettersVector;
+};
+
+class Game
+{
+public:
+	void run()
+	{
+		std::cout << "Welcome to hangman!  You will have 10 guesses to find our word! Good luck!" << std::endl;
+		while (incorrectGuesses < 10)
+		{
+			// this->printGameStatus();
+			this->printGuessesLeft();
+			bool isCorrectGuess = this->word.guessLetter();
+			if (isCorrectGuess)
+			{
+				continue;
+			}
+			else
+			{
+				this->incorrectGuesses++;
+			}
+		}
+		std::cout << "You lose!" << std::endl;
+	}
+
+private:
+	Word word;
+	int incorrectGuesses = 0;
+	std::vector<std::string> display{"  | ", "  | ", " [ ", "]", "  | ", "-- ", "-- ", "  | ", " /", " \\"};
+	void printGameStatus()
+	{
+		for (int i = 0; i < this->incorrectGuesses; i++)
+		{
+			if (i != 2 && i != 5 && i != 8)
+			{
+				std::cout << this->display[i] << std::endl;
+			}
+			else
+			{
+				std::cout << this->display[i];
+			}
+		}
+	}
+	void printGuessesLeft()
+	{
+		std::cout << "Guesses left: " << (10 - this->incorrectGuesses) << std::endl;
+	}
 };
 
 int main()
 {
 	Game hangman;
-	Word word;
-
 	hangman.run();
-	// word.printWordStatus();
 	return 0;
 }
